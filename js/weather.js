242fd61196ffcea7b027ac26a8f6ac57434fab4b
yult97 <1904282367@qq.com>
2022-09-02 22:08:35 +0800

    Site updated: 2022-09-02 22:08:34

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+//天气插件样式
+WIDGET = {
+    "CONFIG": {
+        "modules": "01234",
+        "background": "5",
+        "tmpColor": "FF9900",
+        "tmpSize": "16",
+        "cityColor": "FFFFFF",
+        "citySize": "16",
+        "aqiColor": "FFFFFF",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 10px 10px 10px",
+        "shadow": "0",
+        "language": "auto",
+        "borderRadius": "5",
+        "fixed": "false",
+        "vertical": "center",
+        "horizontal": "center",
+        "key": "fa27238368e7404faabea8ed9fc58301"
+    }
+}
